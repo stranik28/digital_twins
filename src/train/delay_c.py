5ff60d8e5983f5e16387b98ea859acfc7a9f986a
stranik28 <jb357506@gmail.com>
2023-06-18 15:33:20 +0300

    final

--- src/train/delay_c.py
+++ src/train/delay_c.py
@@ -3,10 +3,22 @@ from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from celery import Celery
 from celery.schedules import crontab
-from forecast.forcast import ForcastRepoitory
 import pandas as pd
 
-celery = Celery("train_model", broker=f"redis://redis:6379/0")
+celery = Celery("delay_c", broker=f"redis://redis:6379/0")
+
+
+def get_clean_data(df1_q = None, df_q = None) -> pd.DataFrame:
+        if df1_q is not None  or df_q is not None:
+            df1 = pd.read_csv("models/230 old december.csv")
+            df = pd.read_csv("models/230 old march.csv")
+        else:
+            df1 = pd.read_csv("clean/normalized_data/231_december_upcoming_lanes.csv")
+            df = pd.read_csv("clean/normalized_data/237_march_upcoming_lanes.csv")
+        df1_just_numb = df1.loc[df1.index % 5 == 0]
+        df1_just_numb = df1_just_numb.iloc[:5311]
+        df_just_numb = df.loc[df.index % 5 == 0]
+        return df_just_numb, df1_just_numb
 
 # @cache(3600)
 def culculate_sarima(train, must = None):
@@ -23,7 +35,9 @@ def culculate_arima(train, must = None):
 
 @celery.task
 def model_culc():
-    df_a = ForcastRepoitory.get_clean_data(df1, df2)[0]
+    df1 = 1
+    df2 = 2
+    df_a = get_clean_data(df1, df2)[0]
          # Преобразование колонки 'Время' в формат времени
     df_a['Время'] = pd.to_datetime(df_a['Время'])
     df_a.fillna(method='ffill', inplace=True)
